0f41f96dd6a33fa055bf703542bfef2bcd7910e6
esplanadeA <anna.li.dev@gmail.com>
2024-03-05 21:09:55 +1300

    card detail update can slide NOT functioning

--- script.js
+++ script.js
@@ -10,10 +10,8 @@ var imageUrls = [
 
 // Get all card elements
 var cards = document.querySelectorAll('.card');
-console.log(cards);
 cards.forEach(function (card, index) {
   var img = card.querySelector('.card-img-top');
-  console.log(img);
   img.src = imageUrls[index]; // Assign the image URL to the src attribute of the img element
 });
 
